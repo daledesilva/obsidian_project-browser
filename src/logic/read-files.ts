f480f561023c48039f56de563feb315722a8801d
Dale de Silva <daledesilva@gmail.com>
2024-02-18 21:58:03 +1100

    Slightly refined excerpt

--- src/logic/read-files.ts
+++ src/logic/read-files.ts
@@ -1,25 +1,42 @@
-import { TAbstractFile, TFile } from "obsidian";
+import { TAbstractFile, TFile, TFolder } from "obsidian";
 
 /////////
 /////////
 
 export const fetchExcerpt = async (item: TAbstractFile) => {
     const v = item.vault;
-    let excerpt;
+    let excerpt: string = '';
 
     // TODO: Use shortSummary property if present
     // Otherwise do all below (createExcerpt)
 
     if(item instanceof TFile) {
         excerpt = await v.cachedRead(item);
-    } else {
-        excerpt = 'folder';
+        excerpt = removeCodeBlocks(excerpt);
+        excerpt = simplifyWhiteSpace(excerpt);
+
+    } else if(item instanceof TFolder) {
+        for(let i=0; i<item.children.length; i++) {
+            const childItem = item.children[i];
+            if(i>0) excerpt += ', ';   // TODO: Should be a break
+            excerpt += childItem.name;
+        };
+        
     }
 
-    // TODO:
-    // Remove an code blocks
-    // Collapse lines
-
-
     return excerpt;
+}
+
+
+// REVIEW: Review this chat GPT function
+function removeCodeBlocks(text: string): string {
+    const sectionRegex = /(\s*)```([^`]+?)```(\s*)/g;
+    return text.replace(sectionRegex, "");
+}
+
+
+// REVIEW: This isn't properly working with new lines across code blocks and maybe more
+function simplifyWhiteSpace(text: string): string {
+    const lineBreakRegex = /(\\n|\\n\s+|\s+\\n)+/;
+    return text.replace(lineBreakRegex, '. ');
 }
