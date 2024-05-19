a27ca49fd35422572c1a049237f7c3e4ade7d792
Dale de Silva <daledesilva@gmail.com>
2024-05-19 22:59:43 +1000

    Enabled unknown states appearing in browse tab

--- src/logic/section-processes.ts
+++ src/logic/section-processes.ts
@@ -12,28 +12,39 @@ export interface Section {
     settings: FolderSettings | StateSettings_0_0_5,
 }
 
-export const orderSections = (sections: Section[], plugin: ProjectBrowserPlugin): Section[] => {
-    const intendedOrder = plugin.settings.states.visible.map( stateSettings => stateSettings.name );
-    intendedOrder.reverse();
-    intendedOrder.unshift('folders');
-    intendedOrder.push(' ');
-    
-    // Create a map to store the original index of each section title
-    const sectionMap = sections.reduce((accumulator, section, index) => {
-        accumulator[section.title] = index;
-        return accumulator;
-    }, {} as Record<string, number>);
-    
-    // Sort the sections based on their intended order
-    let sortedSections = intendedOrder.map((name) => sections[sectionMap[name]]) || [];
+export const orderSections = (unorderedSections: Section[], plugin: ProjectBrowserPlugin): Section[] => {
+    const hiddenStatesNames = plugin.settings.states.hidden.map( stateSettings => stateSettings.name );
+    const orderReference = plugin.settings.states.visible.map( stateSettings => stateSettings.name );
+    orderReference.reverse();
+    orderReference.unshift('folders');
+    orderReference.push(' ');
+    let remainingSections = unorderedSections.map( section => section );  // duplicate the array to prevent side effects;
+        
+    // Add the sections in their intended order
+    let orderedSections: Section[] = [];
 
-    for(let i=sortedSections.length-1; i>=0; i--) {
-        if(sortedSections[i] === undefined){
-            sortedSections.splice(i,1);
+    orderReference.forEach((name) => {
+        let foundIndex: undefined | number;
+        for(let i=0; i<remainingSections.length; i++) {
+            if(remainingSections[i].title === name) {
+                foundIndex = i;
+                break;
+            }
         }
-    }
+        if(foundIndex === undefined) return;
+
+        const nextSection = remainingSections.splice(foundIndex, 1)[0];
+        orderedSections.push(nextSection);
+    });
+
+    // Filter out any hidden states
+    remainingSections = remainingSections.filter( section => {
+        return !hiddenStatesNames.some( hiddenStateName => hiddenStateName === section.title )
+    })
     
-    return sortedSections;
+    orderedSections.push(...remainingSections);
+    
+    return orderedSections;
 }
 
 
