163522e771a848da260572fa6a2fdd82cfd2329b
Dale de Silva <daledesilva@gmail.com>
2024-09-29 01:36:34 +1000

    Styled hidden folders when visible and made folder context menu option dynamic

--- src/context-menus/folder-context-menu.tsx
+++ src/context-menus/folder-context-menu.tsx
@@ -2,7 +2,7 @@ import { Menu, TFolder } from "obsidian";
 import { deleteFolderWithConfirmation, renameFileOrFolderInPlace, renameFolderInPlace } from "src/logic/file-processes";
 import ProjectBrowserPlugin from "src/main";
 import { RenameFolderModal } from "src/modals/rename-folder-modal/rename-folder-modal";
-import { hideFolder, unhideFolder } from "src/utils/file-manipulation";
+import { getFolderSettings, hideFolder, unhideFolder } from "src/utils/file-manipulation";
 
 ////////
 ////////
@@ -15,11 +15,12 @@ interface registerFolderContextMenuProps {
 }
 
 export function registerFolderContextMenu(props: registerFolderContextMenuProps) {
-    
-    props.folderBtnEl.addEventListener('contextmenu', function(event) {
 
+    props.folderBtnEl.addEventListener('contextmenu', async (event) => {
         // Prevent container divs opening their context menus
         event.stopPropagation();
+        
+        const folderSettings = await getFolderSettings(props.plugin.app.vault, props.folder)
 
         // Close other menus (Only works on iOS for some reason, but also only needed there)
         document.body.click();
@@ -32,20 +33,23 @@ export function registerFolderContextMenu(props: registerFolderContextMenuProps)
                     props.plugin.saveSettings();
                 })
         );
-        menu.addItem((item) =>
-            item.setTitle("Hide folder")
-                .onClick(async () => {
-                    await hideFolder(props.plugin, props.folder);
-                    props.onFolderChange();
-                })
-        );
-        menu.addItem((item) =>
-            item.setTitle("Unhide folder")
-                .onClick(async () => {
-                    await unhideFolder(props.plugin, props.folder);
-                    props.onFolderChange();
-                })
-        );
+        if(folderSettings.isHidden) {
+            menu.addItem((item) =>
+                item.setTitle("Unhide folder")
+                    .onClick(async () => {
+                        await unhideFolder(props.plugin, props.folder);
+                        props.onFolderChange();
+                    })
+            );
+        } else {
+            menu.addItem((item) =>
+                item.setTitle("Hide folder")
+                    .onClick(async () => {
+                        await hideFolder(props.plugin, props.folder);
+                        props.onFolderChange();
+                    })
+            );
+        }
         menu.addSeparator();
         menu.addItem((item) =>
             item.setTitle("Rename folder")
