7fbbfc854e222f6d02f0b95da9024da1a6093358
Dale de Silva <daledesilva@gmail.com>
2024-12-31 15:31:41 +1100

    Updated intern tag to internal-test & adjusted workflow titles

--- scripts/internal-release.sh
+++ scripts/internal-release.sh
@@ -4,13 +4,13 @@
 git fetch --prune origin "+refs/tags/*:refs/tags/*"
 
 # Delete local tag if it exists
-git tag -d internal
+git tag -d internal-test
 
 # Delete remote tag if it exists
-git push origin --delete internal
+git push origin --delete internal-test
 
 # Create new tag
-git tag internal
+git tag internal-test
 
 # Push the new tag
 git push origin --tags 
