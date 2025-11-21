628863efc7ac963eefe24f6a1c144c563b2375ac
Hasan-01 <mdh4179@gmail.com>
2025-11-21 22:01:05 +0600

    Added Tag-based navigation mode

--- src/components/card-browser/card-browser.tsx
+++ src/components/card-browser/card-browser.tsx
@@ -1,195 +1,243 @@
-import './card-browser.scss';
+import "./card-browser.scss";
 import * as React from "react";
 import { FolderSection } from "../section/folder-section";
 import { StateSection } from "../section/state-section";
 import { StatelessSection } from "../section/stateless-section";
-import { TFile, TFolder } from 'obsidian';
-import { BackButtonAndPath } from '../back-button-and-path/back-button-and-path';
-import { filterSectionsByString, getSortedSectionsInFolder, orderItemsInSections } from 'src/logic/folder-processes';
-import { CardBrowserViewEState, CardBrowserViewState, PartialCardBrowserViewState } from 'src/views/card-browser-view/card-browser-view';
-import { v4 as uuidv4 } from 'uuid';
-import { registerCardBrowserContextMenu } from 'src/context-menus/card-browser-context-menu';
-import { getGlobals } from 'src/logic/stores';
-import { SearchInput } from '../search-input/search-input';
-import classNames from 'classnames';
-import { CardBrowserFloatingMenu } from '../card-browser-floating-menu/card-browser-floating-menu';
+import { TFile, TFolder } from "obsidian";
+import { BackButtonAndPath } from "../back-button-and-path/back-button-and-path";
+import {
+  filterSectionsByString,
+  getSortedSectionsInFolder,
+  orderItemsInSections,
+} from "src/logic/folder-processes";
+// NEW: Import tag processes
+import { getSortedSectionsByTag, TagFolder } from "src/logic/tag-processes";
+import {
+  CardBrowserViewEState,
+  CardBrowserViewState,
+  PartialCardBrowserViewState,
+} from "src/views/card-browser-view/card-browser-view";
+import { v4 as uuidv4 } from "uuid";
+import { registerCardBrowserContextMenu } from "src/context-menus/card-browser-context-menu";
+import { getGlobals } from "src/logic/stores";
+import { SearchInput } from "../search-input/search-input";
+import classNames from "classnames";
+import { CardBrowserFloatingMenu } from "../card-browser-floating-menu/card-browser-floating-menu";
 
 //////////
 //////////
 
 export const CardBrowserContext = React.createContext<{
-    folder: null | TFolder,
-    lastTouchedFilePath: string,
-    rememberLastTouchedFile: (file: TFile) => void,
-    openFolderInSameLeaf: (folder: TFolder) => void,
-    rerender: () => void,
+  // Changed from TFolder to any to accept TagFolder without complex Typescript refactoring
+  folder: null | TFolder | any;
+  lastTouchedFilePath: string;
+  rememberLastTouchedFile: (file: TFile) => void;
+  openFolderInSameLeaf: (folder: TFolder | any) => void;
+  rerender: () => void;
 }>({
-    folder: null,
-    lastTouchedFilePath: '',
-    rememberLastTouchedFile: () => {},
-    openFolderInSameLeaf: () => {},
-    rerender: () => {},
+  folder: null,
+  lastTouchedFilePath: "",
+  rememberLastTouchedFile: () => {},
+  openFolderInSameLeaf: () => {},
+  rerender: () => {},
 });
 
 export interface CardBrowserHandlers {
-    rerender: Function,
+  rerender: Function;
 }
 // export const cardBrowserHandlers = atom<CardBrowserHandlers>()
 
 interface CardBrowserProps {
-    path: string,
-    setViewStateWithHistory: (viewState: PartialCardBrowserViewState) => void,
-    rememberLastTouchedFilepath: (filepath: string) => {},
-    resetLastTouchedFilepath: Function,
-    getViewStates: () => {state: CardBrowserViewState, eState: CardBrowserViewEState},
-    passBackHandlers: (handlers: CardBrowserHandlers) => void,
+  path: string;
+  setViewStateWithHistory: (viewState: PartialCardBrowserViewState) => void;
+  rememberLastTouchedFilepath: (filepath: string) => {};
+  resetLastTouchedFilepath: Function;
+  getViewStates: () => {
+    state: CardBrowserViewState;
+    eState: CardBrowserViewEState;
+  };
+  passBackHandlers: (handlers: CardBrowserHandlers) => void;
 }
 
 export const CardBrowser = (props: CardBrowserProps) => {
-    const {plugin} = getGlobals();
-    const [viewInstanceId] = React.useState<string>(uuidv4());
-    const [refreshId, setRefreshId] = React.useState<number>(uuidv4());
-    const [searchActive, setSearchActive] = React.useState<boolean>(false);
-    const [searchStr, setSearchStr] = React.useState<string>('');
-    const {state, eState} = props.getViewStates();
-    const browserRef = React.useRef(null);
+  const { plugin } = getGlobals();
+  const [viewInstanceId] = React.useState<string>(uuidv4());
+  const [refreshId, setRefreshId] = React.useState<number>(uuidv4());
+  const [searchActive, setSearchActive] = React.useState<boolean>(false);
+  const [searchStr, setSearchStr] = React.useState<string>("");
+  const { state, eState } = props.getViewStates();
+  const browserRef = React.useRef(null);
 
-    // const setCardBrowserHandlers = useSetAtom(cardBrowserHandlers);
+  // const setCardBrowserHandlers = useSetAtom(cardBrowserHandlers);
 
-    // const [files, setFiles] = useState
-    const v = plugin.app.vault;
-    // const [path, setPath] = React.useState(props.path);
-    const initialFolder = v.getFolderByPath(state.path) || v.getRoot(); // TODO: Check this is valid?
-    let sectionsOfItems = getSortedSectionsInFolder(initialFolder);
-    
-    filterSectionsByString(sectionsOfItems, searchStr);
-    
-    const lastTouchedFilePath = eState?.lastTouchedFilePath || '';
+  // const [files, setFiles] = useState
+  const v = plugin.app.vault;
 
-    // on mount
-    React.useEffect( () => {
-        if(!plugin) return;
-        
-        props.passBackHandlers({
-            rerender,
-        })
-        plugin.addGlobalFileDependant(`card-browser_${viewInstanceId}`, rerender);
+  // NEW: Logic to switch between Tag mode and Folder mode
+  let initialFolder: any;
+  let sectionsOfItems = [];
 
-        // NOTE: When the view is changed to something else, this is never given the chance to unmount.
-        // Must removeDependant from elsewhere?
-        // return;
+  if (plugin.settings.browserMode === "tag") {
+    // TAG MODE
+    // Treat path as tag path (e.g. "project/marketing")
+    const tagPath =
+      state.path === "/" || state.path === "root" ? "" : state.path;
 
-        if(plugin && browserRef.current) {
-            registerCardBrowserContextMenu(browserRef.current, initialFolder, {
-                openFile: () => {}, // TODO: maybe remove this... it used to be a function
-                getCurFolder,
-            });
-        }
-        
-    },[])
+    sectionsOfItems = getSortedSectionsByTag(tagPath);
 
-    const getCurFolder = (): TFolder => {
-        const curFolder = v.getFolderByPath(props.getViewStates().state.path) || v.getRoot();
-        return curFolder;
-    }
-
-    function rerender() {
-        setRefreshId(uuidv4());
-    }
-    
-    return (
-        <CardBrowserContext.Provider value={{
-            folder: initialFolder,
-            lastTouchedFilePath,
-            openFolderInSameLeaf,
-            rememberLastTouchedFile,
-            rerender,
-        }}>
-            <div
-                ref = {browserRef}
-                className = 'ddc_pb_browser'
-            >
-                <BackButtonAndPath
-                    folder = {initialFolder}
-                    onBackClick = {openParentFolder}
-                    onFolderClick = { (folder: TFolder) => openFolderInSameLeaf(folder)}
-                />
-                <div
-                    className = {classNames([
-                        'ddc_pb_section',
-                        'ddc_pb_nav-and-filter-section'
-                    ])}
-                >
-                    {sectionsOfItems.map( (section) => (
-                        <React.Fragment key={section.title}>
-                            {section.type === "folders" && (<>
-                                <FolderSection section={section}/>
-                            </>)}
-                        </React.Fragment>
-                    ))}
-                    <SearchInput
-                        searchActive = {searchActive}
-                        onChange = {setSearchStr}
-                        hideSearchInput = {() => setSearchActive(false)}
-                        showSearchInput = {() => setSearchActive(true)}
-                    />
-                </div>
-                <div>
-                    {sectionsOfItems.map( (section, index) => (
-                        <React.Fragment key={section.title}>
-                            {section.type !== "folders" && (
-                                (!searchActive || (searchActive && section.items.length > 0)) && (
-                                    <div>
-                                        {section.type === "state" && (
-                                            <StateSection section={section}/>
-                                        )}
-                                        {section.type === "stateless" && (
-                                            <StatelessSection section={section}/>
-                                        )}
-                                    </div>
-                                )
-                            )}
-                        </React.Fragment>
-                    ))}
-                </div>
-                <CardBrowserFloatingMenu
-                    folder = {initialFolder}
-                    searchActive = {searchActive}
-                    activateSearch = {() => setSearchActive(true)}
-                    deactivateSearch = {() => setSearchActive(false)}
-                />
-            </div>
-        </CardBrowserContext.Provider>
+    // Create a virtual folder object so the UI components (Breadcrumbs/Buttons) have something to read
+    initialFolder = new TagFolder(
+      tagPath === "" ? "All Tags" : tagPath.split("/").pop() || "Tag",
+      tagPath,
+      v,
     );
+  } else {
+    // FOLDER MODE (Original Logic)
+    initialFolder = v.getFolderByPath(state.path) || v.getRoot();
+    sectionsOfItems = getSortedSectionsInFolder(initialFolder);
+  }
 
-    ////////
+  // Filter whatever we found
+  filterSectionsByString(sectionsOfItems, searchStr);
 
-    function rememberLastTouchedFile(file: TFile) {
-        props.rememberLastTouchedFilepath(file.path);
+  const lastTouchedFilePath = eState?.lastTouchedFilePath || "";
+
+  // on mount
+  React.useEffect(() => {
+    if (!plugin) return;
+
+    props.passBackHandlers({
+      rerender,
+    });
+    plugin.addGlobalFileDependant(`card-browser_${viewInstanceId}`, rerender);
+
+    // NOTE: When the view is changed to something else, this is never given the chance to unmount.
+    // Must removeDependant from elsewhere?
+    // return;
+
+    if (plugin && browserRef.current) {
+      // Only register context menu if we are in folder mode (or handle tag mode inside context menu logic later)
+      // For now, passing initialFolder (which might be TagFolder) might limit functionality of the context menu
+      registerCardBrowserContextMenu(browserRef.current, initialFolder, {
+        openFile: () => {}, // TODO: maybe remove this... it used to be a function
+        getCurFolder,
+      });
+    }
+  }, []);
+
+  const getCurFolder = (): TFolder => {
+    const curFolder =
+      v.getFolderByPath(props.getViewStates().state.path) || v.getRoot();
+    return curFolder;
+  };
+
+  function rerender() {
+    setRefreshId(uuidv4());
+  }
+
+  return (
+    <CardBrowserContext.Provider
+      value={{
+        folder: initialFolder,
+        lastTouchedFilePath,
+        openFolderInSameLeaf,
+        rememberLastTouchedFile,
+        rerender,
+      }}
+    >
+      <div ref={browserRef} className="ddc_pb_browser">
+        <BackButtonAndPath
+          folder={initialFolder}
+          onBackClick={openParentFolder}
+          onFolderClick={(folder: TFolder) => openFolderInSameLeaf(folder)}
+        />
+        <div
+          className={classNames([
+            "ddc_pb_section",
+            "ddc_pb_nav-and-filter-section",
+          ])}
+        >
+          {sectionsOfItems.map((section) => (
+            <React.Fragment key={section.title}>
+              {section.type === "folders" && (
+                <>
+                  <FolderSection section={section} />
+                </>
+              )}
+            </React.Fragment>
+          ))}
+          <SearchInput
+            searchActive={searchActive}
+            onChange={setSearchStr}
+            hideSearchInput={() => setSearchActive(false)}
+            showSearchInput={() => setSearchActive(true)}
+          />
+        </div>
+        <div>
+          {sectionsOfItems.map((section, index) => (
+            <React.Fragment key={section.title}>
+              {section.type !== "folders" &&
+                (!searchActive ||
+                  (searchActive && section.items.length > 0)) && (
+                  <div>
+                    {section.type === "state" && (
+                      <StateSection section={section} />
+                    )}
+                    {section.type === "stateless" && (
+                      <StatelessSection section={section} />
+                    )}
+                  </div>
+                )}
+            </React.Fragment>
+          ))}
+        </div>
+        <CardBrowserFloatingMenu
+          folder={initialFolder}
+          searchActive={searchActive}
+          activateSearch={() => setSearchActive(true)}
+          deactivateSearch={() => setSearchActive(false)}
+        />
+      </div>
+    </CardBrowserContext.Provider>
+  );
+
+  ////////
+
+  function rememberLastTouchedFile(file: TFile) {
+    props.rememberLastTouchedFilepath(file.path);
+  }
+
+  // Updated to accept TFolder OR TagFolder
+  function openFolderInSameLeaf(nextFolder: any) {
+    const { plugin } = getGlobals();
+    let { workspace } = plugin.app;
+    let leaf = workspace.getMostRecentLeaf();
+
+    // TODO: Unwrap this (remove if)??
+    if (leaf) {
+      props.resetLastTouchedFilepath();
     }
 
-    function openFolderInSameLeaf(nextFolder: TFolder) {
-        const {plugin} = getGlobals();
-        let { workspace } = plugin.app;
-        let leaf = workspace.getMostRecentLeaf();
-        
-        // TODO: Unwrap this (remove if)??
-        if(leaf) {
-            props.resetLastTouchedFilepath();
-        }
-        
-        props.setViewStateWithHistory({
-            path: nextFolder.path,
-        });
-    }
-    
-    function openParentFolder() {
-        const nextFolder = initialFolder.parent;
-        if(!nextFolder) return;
-        openFolderInSameLeaf(nextFolder);
-    }
+    props.setViewStateWithHistory({
+      path: nextFolder.path,
+    });
+  }
 
-
+  // Updated logic to handle "Parent" of a Tag path (e.g. #tag/sub -> #tag)
+  function openParentFolder() {
+    if (plugin.settings.browserMode === "tag") {
+      if (!state.path || state.path === "/" || state.path === "") return;
+      const segments = state.path.split("/");
+      segments.pop(); // remove last segment
+      const nextPath = segments.join("/");
+      props.setViewStateWithHistory({ path: nextPath });
+    } else {
+      // Original Folder Logic
+      const nextFolder = initialFolder.parent;
+      if (!nextFolder) return;
+      openFolderInSameLeaf(nextFolder);
+    }
+  }
 };
 
 //////////
--- src/types/plugin-settings_0_3_0.ts
+++ src/types/plugin-settings_0_3_0.ts
@@ -1,176 +1,184 @@
-import { FolderSectionSettings_0_0_5, StateViewMode_0_0_5 } from "./plugin-settings_0_0_5";
+import {
+  FolderSectionSettings_0_0_5,
+  StateViewMode_0_0_5,
+} from "./plugin-settings_0_0_5";
 
 ////////////////
 ////////////////
 
 // So it's accessible as a const and a type
 export const StateViewOrder_0_3_0 = {
-    AliasOrFilename: 'Alias or Filename',
-    CreationDate: 'Creation Date',
-    ModifiedDate: 'Modified Date',
+  AliasOrFilename: "Alias or Filename",
+  CreationDate: "Creation Date",
+  ModifiedDate: "Modified Date",
 };
-export type StateViewOrder_0_3_0 = typeof StateViewOrder_0_3_0[keyof typeof StateViewOrder_0_3_0];
+export type StateViewOrder_0_3_0 =
+  (typeof StateViewOrder_0_3_0)[keyof typeof StateViewOrder_0_3_0];
 
 export interface StateSettings_0_3_0 {
-	name: string,
-	link?: boolean,
-	defaultViewMode: StateViewMode_0_0_5,
-	defaultViewOrder: StateViewOrder_0_3_0,
-	defaultViewPriorityVisibility: boolean,
-	defaultViewPriorityGrouping: boolean,
+  name: string;
+  link?: boolean;
+  defaultViewMode: StateViewMode_0_0_5;
+  defaultViewOrder: StateViewOrder_0_3_0;
+  defaultViewPriorityVisibility: boolean;
+  defaultViewPriorityGrouping: boolean;
 }
 
 export const DEFAULT_STATE_SETTINGS_0_3_0: StateSettings_0_3_0 = {
-	name: '',
-	link: true,
-	defaultViewMode: 'Simple Cards',
-	defaultViewOrder: 'AliasOrFilename',
-	defaultViewPriorityVisibility: true,
-	defaultViewPriorityGrouping: true,
-}
+  name: "",
+  link: true,
+  defaultViewMode: "Simple Cards",
+  defaultViewOrder: "AliasOrFilename",
+  defaultViewPriorityVisibility: true,
+  defaultViewPriorityGrouping: true,
+};
 
 export interface PrioritySettings_0_3_0 {
-	name: string,
-	link?: boolean,
+  name: string;
+  link?: boolean;
 }
 
 export const DEFAULT_PRIORITY_SETTINGS_0_3_0: PrioritySettings_0_3_0 = {
-	name: '',
-	link: false,
-}
+  name: "",
+  link: false,
+};
 
 /////////////
 
 export interface PluginSettings_0_3_0 {
-	settingsVersion: string,
-	// Helpers
-    onboardingNotices: {
-		welcomeNoticeRead: boolean,
-		lastVersionNoticeRead: string,
-	},
-	//
-	access: {
-		replaceNewTab: boolean,
-		enableRibbonIcon: boolean,
-		enableCommand: boolean,
-		launchFolder: string,
-	}
-	useAliases: boolean,
-	showStateMenu: boolean,
-	loopStatesWhenCycling: boolean,
-	folders: FolderSectionSettings_0_0_5,
-	states: {
-		visible: StateSettings_0_3_0[],	// changed
-		hidden: StateSettings_0_3_0[],	// changed
-	},
-	stateless: StateSettings_0_3_0,	// changed
-	defaultState?: string,
+  settingsVersion: string;
+  // Helpers
+  onboardingNotices: {
+    welcomeNoticeRead: boolean;
+    lastVersionNoticeRead: string;
+  };
+  //
+  access: {
+    replaceNewTab: boolean;
+    enableRibbonIcon: boolean;
+    enableCommand: boolean;
+    launchFolder: string;
+  };
+  useAliases: boolean;
+  showStateMenu: boolean;
+  loopStatesWhenCycling: boolean;
+  // NEW: Add browserMode definition here
+  browserMode: "folder" | "tag";
+  folders: FolderSectionSettings_0_0_5;
+  states: {
+    visible: StateSettings_0_3_0[];
+    hidden: StateSettings_0_3_0[];
+  };
+  stateless: StateSettings_0_3_0;
+  defaultState?: string;
 
-	// Tracked but not exposed in settings
-	priorities: PrioritySettings_0_3_0[],	// new
-	defaultPriority?: string,	// new
+  // Tracked but not exposed in settings
+  priorities: PrioritySettings_0_3_0[];
+  defaultPriority?: string;
 }
 
 /////////////
 
 export const DEFAULT_PLUGIN_SETTINGS_0_3_0: PluginSettings_0_3_0 = {
-	// Helpers
-    onboardingNotices: {
-		welcomeNoticeRead: false,
-		lastVersionNoticeRead: '',
-	},
-	settingsVersion: '0.3.0',	// Settings version aligns with the version number of the plugin it was updated in
-	access: {
-		replaceNewTab: true,
-		enableRibbonIcon: true,
-		enableCommand: true,
-		launchFolder: '/',
-	},
-	useAliases: true,
-	showStateMenu: true,
-	loopStatesWhenCycling: true,
-	folders: {
-		defaultView: 'Small',
-	},
-	states: {
-		visible: [
-			{
-				name: 'Idea',
-				link: true,
-				defaultViewMode: 'Small Cards',
-				defaultViewOrder: 'AliasOrFilename',
-				defaultViewPriorityVisibility: true,
-				defaultViewPriorityGrouping: true,
-			},
-			{
-				name: 'Shortlisted',
-				link: true,
-				defaultViewMode: 'Small Cards',
-				defaultViewOrder: 'AliasOrFilename',
-				defaultViewPriorityVisibility: true,
-				defaultViewPriorityGrouping: true,
-			},
-			{
-				name: 'Drafting',
-				link: true,
-				defaultViewMode: 'Detailed Cards',
-				defaultViewOrder: 'AliasOrFilename',
-				defaultViewPriorityVisibility: true,
-				defaultViewPriorityGrouping: true,
-			},
-			{
-				name: 'Focus',
-				link: true,
-				defaultViewMode: 'Simple Cards',
-				defaultViewOrder: 'AliasOrFilename',
-				defaultViewPriorityVisibility: true,
-				defaultViewPriorityGrouping: true,
-			},
-			{
-				name: 'Final',
-				link: true,
-				defaultViewMode: 'Small Cards',
-				defaultViewOrder: 'ModifiedDate',
-				defaultViewPriorityVisibility: false,
-				defaultViewPriorityGrouping: false,
-			},
-		],
-		hidden: [
-			{
-				name: 'Archived',
-				link: true,
-				defaultViewMode: 'Small Cards',
-				defaultViewOrder: 'ModifiedDate',
-				defaultViewPriorityVisibility: false,
-				defaultViewPriorityGrouping: false,
-			},
-			{
-				name: 'Cancelled',
-				link: true,
-				defaultViewMode: 'Detailed Cards',
-				defaultViewOrder: 'ModifiedDate',
-				defaultViewPriorityVisibility: false,
-				defaultViewPriorityGrouping: false,
-			},
-		],
-	},
-	stateless: {
-		name: '',
-		defaultViewMode: 'List',
-		defaultViewOrder: 'ModifiedDate',
-		defaultViewPriorityVisibility: true,
-		defaultViewPriorityGrouping: true,
-	},
-	defaultState: 'Idea',
+  // Helpers
+  onboardingNotices: {
+    welcomeNoticeRead: false,
+    lastVersionNoticeRead: "",
+  },
+  settingsVersion: "0.3.0",
+  access: {
+    replaceNewTab: true,
+    enableRibbonIcon: true,
+    enableCommand: true,
+    launchFolder: "/",
+  },
+  useAliases: true,
+  showStateMenu: true,
+  loopStatesWhenCycling: true,
+  // NEW: Default to folder mode so existing users don't break
+  browserMode: "folder",
+  folders: {
+    defaultView: "Small",
+  },
+  states: {
+    visible: [
+      {
+        name: "Idea",
+        link: true,
+        defaultViewMode: "Small Cards",
+        defaultViewOrder: "AliasOrFilename",
+        defaultViewPriorityVisibility: true,
+        defaultViewPriorityGrouping: true,
+      },
+      {
+        name: "Shortlisted",
+        link: true,
+        defaultViewMode: "Small Cards",
+        defaultViewOrder: "AliasOrFilename",
+        defaultViewPriorityVisibility: true,
+        defaultViewPriorityGrouping: true,
+      },
+      {
+        name: "Drafting",
+        link: true,
+        defaultViewMode: "Detailed Cards",
+        defaultViewOrder: "AliasOrFilename",
+        defaultViewPriorityVisibility: true,
+        defaultViewPriorityGrouping: true,
+      },
+      {
+        name: "Focus",
+        link: true,
+        defaultViewMode: "Simple Cards",
+        defaultViewOrder: "AliasOrFilename",
+        defaultViewPriorityVisibility: true,
+        defaultViewPriorityGrouping: true,
+      },
+      {
+        name: "Final",
+        link: true,
+        defaultViewMode: "Small Cards",
+        defaultViewOrder: "ModifiedDate",
+        defaultViewPriorityVisibility: false,
+        defaultViewPriorityGrouping: false,
+      },
+    ],
+    hidden: [
+      {
+        name: "Archived",
+        link: true,
+        defaultViewMode: "Small Cards",
+        defaultViewOrder: "ModifiedDate",
+        defaultViewPriorityVisibility: false,
+        defaultViewPriorityGrouping: false,
+      },
+      {
+        name: "Cancelled",
+        link: true,
+        defaultViewMode: "Detailed Cards",
+        defaultViewOrder: "ModifiedDate",
+        defaultViewPriorityVisibility: false,
+        defaultViewPriorityGrouping: false,
+      },
+    ],
+  },
+  stateless: {
+    name: "",
+    defaultViewMode: "List",
+    defaultViewOrder: "ModifiedDate",
+    defaultViewPriorityVisibility: true,
+    defaultViewPriorityGrouping: true,
+  },
+  defaultState: "Idea",
 
-	priorities: [
-		{
-			...DEFAULT_PRIORITY_SETTINGS_0_3_0,
-			name: 'High',
-		},
-		{
-			...DEFAULT_PRIORITY_SETTINGS_0_3_0,
-			name: 'Low',
-		},
-	],
-}
+  priorities: [
+    {
+      ...DEFAULT_PRIORITY_SETTINGS_0_3_0,
+      name: "High",
+    },
+    {
+      ...DEFAULT_PRIORITY_SETTINGS_0_3_0,
+      name: "Low",
+    },
+  ],
+};
